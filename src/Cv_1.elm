feb48378ec7e4beb17ba8dcd8706202acc25f9c7
Dobroslav Tokoš <dtokos@zoznam.sk>
2020-03-03 14:45:55 +0100

    Fixed indentation

--- src/Cv_1.elm
+++ src/Cv_1.elm
@@ -3,23 +3,23 @@ module Cv_1 exposing (..)
 import Html exposing (text)
 
 main =
-  --text <| fromBoolIf (5 > 6)
-  --text <| fromBoolCase (5 > 6)
-  text <| fromTuple(5, "ahoj")
+    --text <| fromBoolIf (5 > 6)
+    --text <| fromBoolCase (5 > 6)
+    text <| fromTuple(5, "ahoj")
 
 fromBoolIf : Bool -> String
 fromBoolIf x =
-  if x == True then
-    "True"
-  else
-    "False"
+    if x == True then
+        "True"
+    else
+        "False"
 
 fromBoolCase : Bool -> String
 fromBoolCase x =
-  case x of
-    True -> "True"
-    False -> "False"
+    case x of
+        True -> "True"
+        False -> "False"
 
 fromTuple : (Int, String) -> String
 fromTuple (a, b) =
-  (String.fromInt a) ++ ", " ++ b
+    (String.fromInt a) ++ ", " ++ b
